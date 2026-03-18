7463ca6fd9e6bad79fe82ff9f4b7eeedf8ca783a
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-03-18 13:04:23 +0530

    Update the UI of the parent activity

--- MyApplicationMCA/app/src/main/java/com/example/myapplicationmca/Unit2Activity.java
+++ MyApplicationMCA/app/src/main/java/com/example/myapplicationmca/Unit2Activity.java
@@ -1,6 +1,10 @@
 package com.hamida.myapplicationmca;
 
+import android.content.Intent;
+import android.net.LinkAddress;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Unit2Activity extends AppCompatActivity {
 
+    LinearLayout layout1,layout2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_unit2);
+
+        layout1 = findViewById(R.id.layout1);
+        layout2 = findViewById(R.id.layout2);
+
+        layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Unit2Activity.this , LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        layout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Unit2Activity.this , LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
