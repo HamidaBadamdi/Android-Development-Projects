9db5fc64c97aef530da26c92ff71164898ee0942
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-05-13 14:05:33 +0530

    Add DigitalClock with live system time display
    - Implemented DigitalClock Activity using TextClock widget
    - Displayed live system time in real-time
    - Added support for both 12-hour and 24-hour formats
    - Included title and informational TextView
    - Improved user interface with readable typography and spacing

---
+++ MyApplicationMCA/app/src/main/java/com/example/myapplicationmca/DigitalClockActivity.java
@@ -0,0 +1,20 @@
+package com.hamida.myapplicationmca;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DigitalClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_digital_clock);
+
+    }
+}
